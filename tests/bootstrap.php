36c798b2e17f9da8275f2a65929e9e18757721bb
hidden <zzdhidden@gmail.com>
2013-02-25 10:48:46 +0800

    Doc for phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ *  http://www.phpunit.de/manual/current/en/phpunit-book.html
+ *
+ */
+
 set_include_path(dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
 require '../../vendor/autoload.php';
 require_once 'Slim/Admin.php';
